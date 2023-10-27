7be284c57c5eb6875d45ffc8dcf186a7e4b851b7
Pierre BOUFFIES <115778551+Pierre095@users.noreply.github.com>
2023-10-27 17:49:23 +0200

    Delete orm directory

--- orm/mainapp/views/__init__.py
+++
@@ -1,7 +0,0 @@
-# Pour chaque fichier de vue, il faut en importer toutes les variables ici
-from .home import *
-from .about import *
-from .stadiums import *
-from .newsletter import *
-from .update import *
-from .teams import *
