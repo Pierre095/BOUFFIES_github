4f9985d6e6449297fef6f1885573f1fa933f8337
Pierre BOUFFIES <bouffies.pierre@gmail.com>
2024-03-04 01:16:50 +0100

    avancement majeur

--- bouffies_pierre_sokoban/jeu.js
+++ bouffies_pierre_sokoban/jeu.js
@@ -4,8 +4,20 @@ const context = canvas.getContext('2d');
 const boxSize = 100; // Ajustement pour l'exemple, ajustez selon la taille de votre canvas
 
 key_game_check = false;
+end = false;
+map_count = 1;
 
-
+const moveLimits = {
+    map1: 23,//23
+    map2: 20,//24
+    map3: 25,//32
+    map4: 21,//23
+    map5: 23,//23
+    map6: 43,//43
+    map7: 32,//32
+    map8: 33,//33
+    // Ajoutez d'autres niveaux au besoin
+};
 
 // Vide : 0
 let PJ = []; // 3
@@ -14,14 +26,117 @@ let obstacles_immobiles = []; // 1
 let key_game = [] // 4
 let door = [] // 5
 let finish = [] // 6
-let fi = [] //8
 let mob = [] // 7
 
+
+const map = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map1 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 3, 1, 1],
+    [1, 1, 1, 0, 0, 7, 0, 0, 1, 1],
+    [1, 1, 1, 0, 7, 0, 7, 1, 1, 1],
+    [1, 1, 0, 0, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 2, 0, 0, 2, 0, 1, 1],
+    [1, 1, 0, 2, 0, 2, 0, 6, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
 
 const map2 = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 7, 1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 1, 1, 2, 2, 2, 1, 1],
+    [1, 0, 0, 1, 1, 0, 0, 0, 1, 1],
+    [1, 3, 0, 1, 1, 6, 7, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 0, 7, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map3 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 6, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 5, 1, 1],
+    [1, 1, 1, 0, 0, 0, 0, 0, 3, 1],
+    [1, 1, 1, 0, 1, 0, 1, 0, 0, 1],
+    [1, 1, 1, 0, 0, 7, 0, 0, 0, 1],
+    [1, 4, 1, 0, 1, 0, 1, 0, 1, 1],
+    [1, 0, 0, 0, 0, 0, 7, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map4 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 3, 1, 4, 0, 2, 1, 1, 1, 1],
+    [1, 0, 2, 0, 2, 0, 5, 0, 1, 1],
+    [1, 2, 0, 2, 0, 2, 2, 6, 0, 1],
+    [1, 0, 2, 0, 2, 0, 2, 2, 0, 1],
+    [1, 1, 0, 2, 0, 2, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map5 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 6, 0, 1, 1, 1],
+    [1, 1, 1, 1, 0, 5, 2, 0, 1, 1],
+    [1, 1, 3, 1, 0, 0, 2, 0, 1, 1],
+    [1, 1, 0, 1, 0, 0, 0, 0, 1, 1],
+    [1, 1, 7, 1, 2, 2, 2, 2, 1, 1],
+    [1, 1, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 4, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map6 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 0, 3, 0, 1, 1, 1, 1, 1],
+    [1, 1, 2, 2, 2, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 4, 1, 1, 1, 1, 1],
+    [1, 1, 0, 2, 0, 0, 1, 1, 1, 1],
+    [1, 1, 7, 1, 2, 2, 0, 0, 1, 1],
+    [1, 1, 0, 0, 2, 0, 7, 1, 1, 1],
+    [1, 1, 1, 1, 1, 5, 2, 0, 1, 1],
+    [1, 1, 1, 1, 1, 6, 0, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map7 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 6, 0, 1, 1],
+    [1, 1, 1, 1, 1, 0, 5, 0, 1, 1],
+    [1, 1, 0, 4, 1, 2, 2, 2, 1, 1],
+    [1, 1, 7, 2, 0, 7, 2, 0, 1, 1],
+    [1, 1, 0, 1, 7, 0, 0, 3, 1, 1],
+    [1, 1, 0, 1, 1, 0, 1, 1, 1, 1],
+    [1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+
+const map8 = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 0, 6, 0, 1, 1, 1, 1],
     [1, 1, 1, 2, 5, 2, 1, 1, 1, 1],
     [1, 2, 1, 2, 0, 0, 1, 0, 1, 1],
     [2, 0, 0, 2, 2, 2, 0, 0, 4, 1],
@@ -31,14 +146,11 @@ const map2 = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
-
-
-
-const map = [
+const maptest = [
     [1, 0, 7, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 1, 1, 1, 1, 1, 1, 1],
     [3, 0, 0, 2, 6, 1, 1, 1, 1, 1],
-    [0, 0, 7, 4, 0, 0, 0, 1, 1, 1],
+    [0, 2, 7, 4, 5, 0, 0, 1, 1, 1],
     [1, 2, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -48,7 +160,7 @@ const map = [
 ];
 
 
-canvas.width = map[0].length * boxSize;
+canvas.width = map.length * boxSize;
 canvas.height = map.length * boxSize;
 
 const image_personnage_1 = new Image();
@@ -61,75 +173,86 @@ const image_finish = new Image();
 const image_mob = new Image();
 const image_mob_dead = new Image();
 
-image_personnage_1.src = 'PJ1.png';
-image_personnage_2.src = 'PJ2.png';
-image_personnage_coup.src = 'PJ_coup.png';
-image_obstacle.src = 'bloc.png';
-image_key.src = "key.png"
-image_door.src = "door.png"
-image_finish.src = "finish.png"
-image_mob.src = "mob.png"
-image_mob_dead.src = "mob_dead.png"
+image_personnage_1.src = "IMG/ASSET/PJ1.png";
+image_personnage_2.src = "IMG/ASSET/PJ2.png";
+image_personnage_coup.src = "IMG/ASSET/PJ_coup.png";
+image_obstacle.src = "IMG/ASSET/bloc.png";
+image_key.src = "IMG/ASSET/key.png"
+image_door.src = "IMG/ASSET/door.png"
+image_finish.src = "IMG/ASSET/finish.png"
+image_mob.src = "IMG/ASSET/mob.png"
+image_mob_dead.src = "IMG/ASSET/mob_dead.png"
 
 
+
+//Animation personnage (statique / push)
 let currentCharacterImage = image_personnage_1;
 let pushing = false;
 if (pushing === false) {
     setInterval(() => {
         currentCharacterImage = (currentCharacterImage === image_personnage_1) ? image_personnage_2 : image_personnage_1;
-        draw(); // Redessinez le jeu pour mettre à jour l'image du personnage
-    }, 500); // Change l'image toutes les 500 millisecondes
+        draw();
+    }, 500);
 } else if (pushing === true) {
     setInterval(() => {
         currentCharacterImage = image_personnage_coup;
-        draw(); // Redessinez le jeu pour mettre à jour l'image du personnage
-    }, 250); // Change l'image toutes les 500 millisecondes
+        draw();
+    }, 250); // 
 }
-
 
 
 
 function generateObstacles(map) {
+    // Vider les tableaux avant de générer de nouveaux obstacles pour éviter des doublons
+    PJ = [];
+    obstacles = [];
+    obstacles_immobiles = [];
+    key_game = [];
+    door = [];
+    finish = [];
+    mob = [];
+
     for (let row = 0; row < map.length; row++) {
         for (let col = 0; col < map[row].length; col++) {
-            if (map[row][col] === 2) { //obstacle mobiles
+            if (map[row][col] === 2) { // Obstacle mobile
                 obstacles.push({ x: col * boxSize, y: row * boxSize });
-            } else if (map[row][col] === 3) { // Perosnnage
+            } else if (map[row][col] === 3) { // Personnage
                 PJ.push({ x: col * boxSize, y: row * boxSize });
-            } else if (map[row][col] === 1) { //obstacles immobile
+            } else if (map[row][col] === 1) { // Obstacle immobile
                 obstacles_immobiles.push({ x: col * boxSize, y: row * boxSize });
-            } else if (map[row][col] === 4) {
+            } else if (map[row][col] === 4) { // Clé
                 key_game.push({ x: col * boxSize, y: row * boxSize });
-            } else if (map[row][col] === 5) {
+            } else if (map[row][col] === 5) { // Porte
                 door.push({ x: col * boxSize, y: row * boxSize });
-            } else if (map[row][col] === 6) {
+            } else if (map[row][col] === 6) { // Fin
                 finish.push({ x: col * boxSize, y: row * boxSize });
-            } else if (map[row][col] === 7) {
+            } else if (map[row][col] === 7) { // Mob
                 mob.push({ x: col * boxSize, y: row * boxSize });
             }
         }
     }
+    initializeMoveCount();
 }
 
-generateObstacles(map);
-
 document.addEventListener("keydown", moov);
-
 let d;
-
 function moov(event) {
     let key = event.keyCode;
-    if (key == 90) { // Z
+    if (key == 90 || key == 38) { // Z
         d = "UP";
-    } else if (key == 81) { // Q
+    } else if (key == 81 || key == 37) { // Q
         d = "LEFT";
-    } else if (key == 83) { // S
+    } else if (key == 83 || key == 40) { // S
         d = "DOWN";
-    } else if (key == 68) { // D
+    } else if (key == 68 || key == 39) { // D
         d = "RIGHT";
     }
-
+    move_count -= 1;
+    updateMoveCountDisplay();
+    moveCount()
     movePlayer(d);
+
+    // console.log(move_count);
 }
 
 function movePlayer() {
@@ -150,6 +273,15 @@ function movePlayer() {
             dx = boxSize;
             break;
     }
+    
+
+    // if (move_count <= 0) {
+    //     alert("Dommage ! Vous avez dépassé le nombre de mouvements autorisés. Recommencez !");
+    //     // Vous pouvez ici réinitialiser le niveau ou effectuer d'autres actions nécessaires
+    // }
+
+
+
 
     let newX = PJ[0].x + dx;
     let newY = PJ[0].y + dy;
@@ -159,6 +291,62 @@ function movePlayer() {
     let doorIndex = getDoorIndex(newX, newY);
     let keyIndex = getKeyIndex(newX, newY);
     let mobIndex = getMobIndex(newX, newY);
+    let finishIndex = getFinishIndex(newX, newY);
+
+
+
+    if (finishIndex !== -1) {
+        PJ[0].x = newX; // Met à jour la position du joueur pour être sur la fin
+        PJ[0].y = newY;
+        // Le joueur a atteint la case d'arrivée
+        finish.splice(doorIndex, 1);
+        // Ici, vous pouvez ajouter toute logique nécessaire pour gérer la victoire,
+        // comme arrêter le jeu, afficher un écran de victoire, etc.
+        // Par exemple, pour arrêter de déplacer le joueur après la victoire :
+        end = true;
+        map_count += 1;
+        if (end === true && map_count === 9) {
+            setTimeout(() => {
+                alert("Félicitations ! Vous avez fini le jeu !");
+                const zone_jeu = document.querySelector('.zone_jeu');
+                const accueil = document.querySelector('.accueil_hide');
+                const movecount = document.querySelector('.moveCount');
+                zone_jeu.classList.add('zone_jeu_hide');
+                zone_jeu.classList.remove('zone_jeu');
+                accueil.classList.add('accueil');
+                accueil.classList.remove('accueil_hide');
+                movecount.classList.add('moveCount_hide');
+                movecount.classList.remove('moveCount');
+            }, 250);
+        } else if (end === true) {
+            setTimeout(() => {
+                alert("Félicitations ! Voulez-vous aller au niveau suivant ?");
+                if (map_count === 2) {
+                    generateObstacles(map2);
+                } else if (map_count === 3) {
+                    generateObstacles(map3);
+                } else if (map_count === 4) {
+                    generateObstacles(map4);
+                } else if (map_count === 5) {
+                    generateObstacles(map5);
+                } else if (map_count === 6) {
+                    generateObstacles(map6);
+                } else if (map_count === 7) {
+                    generateObstacles(map7);
+                } else if (map_count === 8) {
+                    generateObstacles(map8);
+                }
+                move_count = 0;
+                initializeMoveCount();
+                // Ajoutez ici toute autre logique de victoire, comme recharger le jeu ou passer au niveau suivant
+            }, 250);
+            console.log(map_count)
+        }
+
+    }
+
+
+
 
     // Collecter la clé si disponible
     if (keyIndex !== -1 && !isObstacleOnKey(key_game[keyIndex]) && !isMobOnKey(key_game[keyIndex])) {
@@ -198,6 +386,15 @@ function movePlayer() {
                         draw(); // Redessinez avec l'image normale
                     }, 250); // Délai pour afficher l'image de poussée
                 }
+            } else {//fait l'animation de pousser même si c'est pas possible
+                pushing = true;
+                if (pushing === true) {
+                    setTimeout(() => {
+                        pushing = false; // Arrêtez de montrer l'image de poussée
+                        draw(); // Redessinez avec l'image normale
+                    }, 250); // Délai pour afficher l'image de poussée
+                }
+
             }
 
         }
@@ -233,25 +430,6 @@ function getKeyIndex(x, y) {
     return -1;
 }
 
-function isObstacleOnKey(keyPosition) {
-    for (let i = 0; i < obstacles.length; i++) {
-        if (obstacles[i].x === keyPosition.x && obstacles[i].y === keyPosition.y) {
-            return true; // Un obstacle est sur la clé
-        }
-    }
-    return false; // Aucun obstacle sur la clé
-}
-
-function isMobOnKey(keyPosition) {
-    for (let i = 0; i < mob.length; i++) {
-        if (mob[i].x === keyPosition.x && mob[i].y === keyPosition.y) {
-            return true; // Un obstacle est sur la clé
-        }
-    }
-    return false; // Aucun obstacle sur la clé
-}
-
-
 function getDoorIndex(x, y) {
     for (let i = 0; i < door.length; i++) {
         if (door[i].x === x && door[i].y === y) {
@@ -279,14 +457,13 @@ function getMobIndex(x, y) {
     return -1;
 }
 
-
-
 function pushObstacle(index, dx, dy) {
     let newX = obstacles[index].x + dx;
     let newY = obstacles[index].y + dy;
 
     // Recherche si une porte est à l'emplacement cible
     let doorAtNewLocationIndex = door.findIndex(d => d.x === newX && d.y === newY);
+    let finishAtNewLocationIndex = finish.findIndex(d => d.x === newX && d.y === newY);
 
     // Recherche si un mob est à l'emplacement cible
     let mobAtNewLocationIndex = mob.findIndex(m => m.x === newX && m.y === newY);
@@ -297,7 +474,7 @@ function pushObstacle(index, dx, dy) {
         let immobileObstacleAtNewLocationIndex = obstacles_immobiles.findIndex(o => o.x === newX && o.y === newY);
 
         // Empêche le déplacement si un autre obstacle, mob, ou la porte se trouve déjà à l'emplacement cible
-        if (obstacleAtNewLocationIndex === -1 && immobileObstacleAtNewLocationIndex === -1 && mobAtNewLocationIndex === -1 && doorAtNewLocationIndex === -1) {
+        if (obstacleAtNewLocationIndex === -1 && immobileObstacleAtNewLocationIndex === -1 && mobAtNewLocationIndex === -1 && doorAtNewLocationIndex === -1 && finishAtNewLocationIndex === -1) {
             obstacles[index].x = newX;
             obstacles[index].y = newY;
             return true; // L'obstacle a été déplacé avec succès
@@ -335,6 +512,112 @@ function pushMob(index, dx, dy) {
     }
 }
 
+function isObstacleOnKey(keyPosition) {
+    for (let i = 0; i < obstacles.length; i++) {
+        if (obstacles[i].x === keyPosition.x && obstacles[i].y === keyPosition.y) {
+            return true; // Un obstacle est sur la clé
+        }
+    }
+    return false; // Aucun obstacle sur la clé
+}
+
+function isMobOnKey(keyPosition) {
+    for (let i = 0; i < mob.length; i++) {
+        if (mob[i].x === keyPosition.x && mob[i].y === keyPosition.y) {
+            return true; // Un obstacle est sur la clé
+        }
+    }
+    return false; // Aucun obstacle sur la clé
+}
+
+function moveCount() {
+    if (map_count === 1) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 23;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map1);
+            }, 250);
+        }
+    } else if (map_count === 2) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 24;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map2);
+            }, 250);
+        }
+    } else if (map_count === 3) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 32;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map3);
+            }, 250);
+        }
+    } else if (map_count === 4) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 23;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map4);
+            }, 250);
+        }
+    } else if (map_count === 5) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 23;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map5);
+            }, 250);
+        }
+    } else if (map_count === 6) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 43;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map6);
+            }, 250);
+        }
+    } else if (map_count === 7) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 32;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map7);
+            }, 250);
+        }
+    } else if (map_count === 8) {
+        if (move_count === -1) {
+            alert("Dommage ! Recommence !");
+            move_count = 33;
+            initializeMoveCount()
+            setTimeout(() => {
+                generateObstacles(map8);
+            }, 250);
+        }
+    }
+}
+
+function initializeMoveCount() {
+    // Exemple d'initialisation pour map1
+    move_count = moveLimits[`map${map_count}`]; // Obtenez la limite de mouvements pour le niveau actuel
+    updateMoveCountDisplay(); // Mettez à jour l'affichage
+}
+
+function updateMoveCountDisplay() {
+    document.getElementById('moveCount').innerText = `${move_count}`;
+}
+
+
+
 
 
 function draw() {
@@ -368,16 +651,9 @@ function draw() {
     if (PJ.length > 0 && pushing === false) {
         context.drawImage(currentCharacterImage, PJ[0].x, PJ[0].y, boxSize, boxSize);
     }
-    const imageToDraw = pushing ? image_personnage_coup : image_personnage;
+    const imageToDraw = pushing ? image_personnage_coup : currentCharacterImage;
     context.drawImage(imageToDraw, PJ[0].x, PJ[0].y, boxSize, boxSize);
 }
-document.addEventListener('keydown', function () {
-    // Commence le jeu
-    // Assurez-vous de retirer cet écouteur d'événement une fois que le jeu a commencé pour éviter de redémarrer le jeu à chaque appui sur une touche
-    startGame(); // Supposons que vous avez une fonction startGame pour initialiser et commencer le jeu
-});
-
-
 
 Promise.all([
     new Promise(resolve => { image_personnage_1.onload = resolve; }),
@@ -394,9 +670,20 @@ Promise.all([
 });
 
 
-// Appelle startScreen pour afficher l'écran de démarrage
-
-
 // Assurez-vous que toutes les images sont chargées avant de dessiner
 
+function startGame() {
+    const zone_jeu = document.querySelector('.zone_jeu_hide');
+    const accueil = document.querySelector('.accueil');
+    const movecount = document.querySelector('.moveCount_hide');
+    zone_jeu.classList.remove('zone_jeu_hide');
+    zone_jeu.classList.add('zone_jeu');
+    accueil.classList.remove('accueil');
+    accueil.classList.add('accueil_hide');
+    movecount.classList.remove('moveCount_hide');
+    movecount.classList.add('moveCount');
+    // Retire l'écran de démarrage et lance le jeu
+    generateObstacles(map1); // Exemple de fonction qui pourrait être appelée pour initialiser le jeu
+    draw(); // Redessine le canvas avec le jeu en cours
 
+}
