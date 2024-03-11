a611ffd35208b93aa2f6c4826f9eb19ec482c7c8
Pierre BOUFFIES <bouffies.pierre@gmail.com>
2024-03-11 19:16:04 +0100

    p

--- Portfolio/JS/bilan.js
+++
@@ -1,9 +0,0 @@
-function textwriter(){
-const contentDiv = document.querySelector('.typing_effect');
-contentDiv.innerHTML = '';
-contentDiv.innerHTML = 'ET, ENFIN VOICI LE <span class="mot_page_typing">BILAN</span> DE MON <span class="mot_page_typing">ÉVOLUTION</span>';
-}
-
-setTimeout(() => {
-    textwriter()
-}, 500);
--- Portfolio/JS/entreprise.js
+++
@@ -1,9 +0,0 @@
-function textwriter(){
-    const contentDiv = document.querySelector('.typing_effect');
-    contentDiv.innerHTML = '';
-    contentDiv.innerHTML = 'VOICI MON ENTREPRISE<span class="mot_page_typing"> SERVICE EXPERT ENTREPRISE</span> ';
-    }
-    
-    setTimeout(() => {
-        textwriter()
-    }, 500);
